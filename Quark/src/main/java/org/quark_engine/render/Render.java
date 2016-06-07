c2ea76c2d8428cf7718b77a98dd5ee994a356aa1
Wolftein <wolftein1@gmail.com>
2016-06-07 14:28:33 -0300

    Ordered opengl enumeration(s).

--- Quark/src/main/java/org/quark_engine/render/Render.java
+++ Quark/src/main/java/org/quark_engine/render/Render.java
@@ -38,161 +38,127 @@ public interface Render extends ManageableManager {
      * <code>GLES2</code> encapsulate all feature(s) supported by OpenGL ES 2.0.
      */
     interface GLES2 {
-        int GL_NONE = -1;
-        int GL_ZERO = 0x0000;
-        int GL_ONE = 0x0001;
-        int GL_SRC_COLOR = 0x0300;
-        int GL_ONE_MINUS_SRC_COLOR = 0x0301;
-        int GL_SRC_ALPHA = 0x0302;
-        int GL_ONE_MINUS_SRC_ALPHA = 0x0303;
-        int GL_DST_ALPHA = 0x0304;
-        int GL_ONE_MINUS_DST_ALPHA = 0x0305;
-        int GL_DST_COLOR = 0x0306;
-        int GL_ONE_MINUS_DST_COLOR = 0x0307;
-
-        int GL_FUNC_ADD = 0x8006;
-        int GL_FUNC_SUBTRACT = 0x800A;
-        int GL_FUNC_REVERSE_SUBTRACT = 0x800B;
-
-        int GL_NEAREST = 0x2600;
-        int GL_LINEAR = 0x2601;
-
-        int GL_NEAREST_MIPMAP_NEAREST = 0x2700;
-        int GL_LINEAR_MIPMAP_NEAREST = 0x2701;
-        int GL_LINEAR_MIPMAP_LINEAR = 0x2703;
-
-        int GL_TEXTURE_2D = 0xDE1;
-
-        int GL_REPEAT = 0x2901;
-        int GL_CLAMP_TO_EDGE = 0x812F;
+        int GL_ALWAYS = 0x0207;
+        int GL_ARRAY_BUFFER = 0x8892;
+        int GL_BACK = 0x0405;
+        int GL_BYTE = 0x1400;
         int GL_CLAMP_TO_BORDER = 0x812D;
-        int GL_MIRRORED_REPEAT = 0x8370;
-
-        int GL_RGB = 0x1907;
-        int GL_RGBA = 0x1908;
-
+        int GL_CLAMP_TO_EDGE = 0x812F;
+        int GL_DECREASE = 0x1E03;
+        int GL_DECREASE_WRAP = 0x8508;
         int GL_DEPTH_COMPONENT16 = 0x81A5;
-
-        int GL_POINTS = 0x00;
+        int GL_DST_ALPHA = 0x0304;
+        int GL_DST_COLOR = 0x0306;
+        int GL_DYNAMIC_DRAW = 0x88E8;
+        int GL_ELEMENT_ARRAY_BUFFER = 0x8893;
+        int GL_EQUAL = 0x0202;
+        int GL_FLOAT = 0x1406;
+        int GL_FRAGMENT_SHADER = 0x8B30;
+        int GL_FRONT = 0x0404;
+        int GL_FRONT_BACK = 0x0408;
+        int GL_FUNC_ADD = 0x8006;
+        int GL_FUNC_REVERSE_SUBTRACT = 0x800B;
+        int GL_FUNC_SUBTRACT = 0x800A;
+        int GL_GREATER = 0x0204;
+        int GL_GREATER_EQUAL = 0x0206;
+        int GL_INCREMENT = 0x1E02;
+        int GL_INCREMENT_WRAP = 0x8507;
+        int GL_INVERT = 0x150A;
+        int GL_KEEP = 0x1E00;
+        int GL_LESS = 0x0201;
+        int GL_LESS_EQUAL = 0x0203;
+        int GL_LINEAR = 0x2601;
+        int GL_LINEAR_MIPMAP_LINEAR = 0x2703;
+        int GL_LINEAR_MIPMAP_NEAREST = 0x2701;
         int GL_LINES = 0x01;
         int GL_LINE_LOOP = 0x02;
         int GL_LINE_STRIP = 0x03;
+        int GL_MIRRORED_REPEAT = 0x8370;
+        int GL_NEAREST = 0x2600;
+        int GL_NEAREST_MIPMAP_NEAREST = 0x2700;
+        int GL_NEVER = 0x0200;
+        int GL_NONE = -1;
+        int GL_NOT_EQUAL = 0x0205;
+        int GL_ONE = 0x0001;
+        int GL_ONE_MINUS_DST_ALPHA = 0x0305;
+        int GL_ONE_MINUS_DST_COLOR = 0x0307;
+        int GL_ONE_MINUS_SRC_ALPHA = 0x0303;
+        int GL_ONE_MINUS_SRC_COLOR = 0x0301;
+        int GL_POINTS = 0x00;
+        int GL_REPEAT = 0x2901;
+        int GL_REPLACE = 0x1E01;
+        int GL_RGB = 0x1907;
+        int GL_RGBA = 0x1908;
+        int GL_SHORT = 0x1402;
+        int GL_SRC_ALPHA = 0x0302;
+        int GL_SRC_COLOR = 0x0300;
+        int GL_STATIC_DRAW = 0x88E4;
+        int GL_STREAM_DRAW = 0x88E0;
+        int GL_TEXTURE_2D = 0xDE1;
         int GL_TRIANGLES = 0x04;
         int GL_TRIANGLE_FAN = 0x05;
         int GL_TRIANGLE_STRIP = 0x06;
-
-        int GL_DYNAMIC_DRAW = 0x88E8;
-        int GL_STATIC_DRAW = 0x88E4;
-        int GL_STREAM_DRAW = 0x88E0;
-
-        int GL_ARRAY_BUFFER = 0x8892;
-        int GL_ELEMENT_ARRAY_BUFFER = 0x8893;
-
-        int GL_BYTE = 0x1400;
         int GL_UNSIGNED_BYTE = 0x1401;
-
-        int GL_SHORT = 0x1402;
         int GL_UNSIGNED_SHORT = 0x1403;
-
-        int GL_FLOAT = 0x1406;
-
-        int GL_FRAGMENT_SHADER = 0x8B30;
         int GL_VERTEX_SHADER = 0x8B31;
-
-        int GL_FRONT = 0x0404;
-        int GL_FRONT_BACK = 0x0408;
-        int GL_BACK = 0x0405;
-
-        int GL_KEEP = 0x1E00;
-        int GL_REPLACE = 0x1E01;
-        int GL_INCREMENT = 0x1E02;
-        int GL_INCREMENT_WRAP = 0x8507;
-        int GL_DECREASE = 0x1E03;
-        int GL_DECREASE_WRAP = 0x8508;
-        int GL_INVERT = 0x150A;
-
-        int GL_NEVER = 0x0200;
-        int GL_LESS = 0x0201;
-        int GL_EQUAL = 0x0202;
-        int GL_LESS_EQUAL = 0x0203;
-        int GL_GREATER = 0x0204;
-        int GL_NOT_EQUAL = 0x0205;
-        int GL_GREATER_EQUAL = 0x0206;
-        int GL_ALWAYS = 0x0207;
+        int GL_ZERO = 0x0000;
     }
 
     /**
      * <code>GLES3</code> encapsulate all feature(s) supported by OpenGL ES 3.0.
      */
     interface GLES3 extends GLES2 {
-        int GL_TEXTURE_3D = 0x806F;
-
-        int GL_RED = 0x1903;
-        int GL_RG = 0x8227;
-
-        int GL_R8 = 0x8229;
+        int GL_COPY_READ_BUFFER = 0x8F36;
+        int GL_COPY_WRITE_BUFFER = 0x8F37;
+        int GL_DEPTH_COMPONENT = 0x1902;
+        int GL_DEPTH_COMPONENT24 = 0x81A6;
+        int GL_DEPTH_COMPONENT32 = 0x81A7;
+        int GL_DEPTH_STENCIL = 0x84F9;
+        int GL_DYNAMIC_COPY = 0x88EA;
+        int GL_DYNAMIC_READ = 0x88E9;
+        int GL_HALF_FLOAT = 0x1406;
+        int GL_INT = 0x1404;
+        int GL_MAX = 0x8008;
+        int GL_MIN = 0x8007;
+        int GL_PIXEL_PACK_BUFFER = 0x88EB;
+        int GL_PIXEL_UNPACK_BUFFER = 0x88EC;
         int GL_R16 = 0x822A;
         int GL_R16F = 0x822D;
         int GL_R32F = 0x822E;
-
-        int GL_RG8 = 0x822B;
+        int GL_R8 = 0x8229;
+        int GL_RED = 0x1903;
+        int GL_RG = 0x8227;
         int GL_RG16 = 0x822C;
         int GL_RG16F = 0x822F;
         int GL_RG32F = 0x8230;
-
-        int GL_RGB8 = 0x8051;
+        int GL_RG8 = 0x822B;
         int GL_RGB16 = 0x8054;
         int GL_RGB16F = 0x881B;
         int GL_RGB32F = 0x8815;
-
-        int GL_RGBA8 = 0x8058;
+        int GL_RGB8 = 0x8051;
         int GL_RGBA16 = 0x805B;
         int GL_RGBA16F = 0x881A;
         int GL_RGBA32F = 0x8814;
-
-        int GL_DEPTH_COMPONENT = 0x1902;
-        int GL_DEPTH_STENCIL = 0x84F9;
-
-        int GL_DEPTH_COMPONENT24 = 0x81A6;
-        int GL_DEPTH_COMPONENT32 = 0x81A7;
-
-        int GL_DYNAMIC_READ = 0x88E9;
-        int GL_DYNAMIC_COPY = 0x88EA;
-
-        int GL_STATIC_READ = 0x88E5;
+        int GL_RGBA8 = 0x8058;
         int GL_STATIC_COPY = 0x88E6;
-
-        int GL_STREAM_READ = 0x88E1;
+        int GL_STATIC_READ = 0x88E5;
         int GL_STREAM_COPY = 0x88E2;
-
-        int GL_COPY_READ_BUFFER = 0x8F36;
-        int GL_COPY_WRITE_BUFFER = 0x8F37;
-
-        int GL_PIXEL_PACK_BUFFER = 0x88EB;
-        int GL_PIXEL_UNPACK_BUFFER = 0x88EC;
-
+        int GL_STREAM_READ = 0x88E1;
+        int GL_TEXTURE_3D = 0x806F;
         int GL_TRANSFORM_FEEDBACK_BUFFER = 0x8C8E;
         int GL_UNIFORM_BUFFER = 0x8A11;
-
-        int GL_INT = 0x1404;
         int GL_UNSIGNED_INT = 0x1405;
-        int GL_HALF_FLOAT = 0x1406;
-
-        int GL_MIN = 0x8007;
-        int GL_MAX = 0x8008;
-
     }
 
     /**
      * <code>GLES31</code> encapsulate all feature(s) supported by OpenGL ES 3.1.
      */
     interface GLES31 extends GLES3 {
+        int GL_GEOMETRY_SHADER = 0x8DD9;
         int GL_LINES_ADJACENCY = 0x0A;
         int GL_LINE_STRIP_ADJACENCY = 0x0B;
         int GL_TRIANGLES_ADJACENCY = 0x0C;
         int GL_TRIANGLE_STRIP_ADJACENCY = 0x0D;
-
-        int GL_GEOMETRY_SHADER = 0x8DD9;
     }
 
     /**
@@ -200,17 +166,16 @@ public interface Render extends ManageableManager {
      */
     interface GLES32 extends GLES31 {
         int GL_TEXTURE_BUFFER = 0x8C2A;
-
     }
 
     /**
      * <code>GLESExtension</code> encapsulate all feature(s) supported by extension.
      */
     interface GLESExtension {
-        int S3TC_RGB_DXT1 = 0x83F0;
         int S3TC_RGBA_DXT1 = 0x83F1;
         int S3TC_RGBA_DXT3 = 0x83F2;
         int S3TC_RGBA_DXT5 = 0x83F3;
+        int S3TC_RGB_DXT1 = 0x83F0;
     }
 
     /**
